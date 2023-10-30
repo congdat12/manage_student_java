f5107bf23993b2853b8acf0f53060757cea2c9dc
congdat12 <congdat1291995@gmail.com>
2023-10-30 15:06:56 +0700

    done

---
+++ src/Controller/Controller.java
@@ -0,0 +1,96 @@
+package Controller;
+
+import Model.Student;
+import View.Menu;
+
+import java.util.*;
+
+public class Controller {
+    ArrayList<Student> arr = new ArrayList<>();
+
+    public void addStudent(Student student){
+        //student.input();
+        arr.add(student);
+    }
+
+    public void removeStudentById(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input id");
+        int id = sc.nextInt();
+        for (Student st : arr){
+            if (st.getId() != id){
+                System.out.println("not found ID");
+            }else if(arr.size() <= 1){
+                System.out.println("not valid...please try again");
+            }
+            else {
+                arr.remove(st);
+            }
+        }
+    }
+    public void showAll(){
+        for (Student st : arr){
+//           System.out.println("id: "+ st.id + "name: " + st.name + "address: " +st.address + "diem tb: "+ st.diemTB);
+            System.out.println(st);
+        }
+    }
+
+    public void findStudentById(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input id");
+        int id = sc.nextInt();
+        for (Student st : arr){
+            if (st.getId() == id){
+                showAll();
+            }else {
+                System.out.println("not found student");
+            }
+
+        }
+    }
+    public void sortByMark(){
+        Collections.sort(arr, new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                if (o1.getDiemTB() > o2.getDiemTB())
+                    return 1;
+                else if (o1.getDiemTB() < o2.getDiemTB())
+                    return -1;
+                else
+                    return o1.getName().compareToIgnoreCase(o2.getName());
+            }
+        });
+
+    }
+    public void menuController(){
+        Menu menu = new Menu();
+        menu.menuController();
+        boolean isChecked = true;
+        Scanner sc = new Scanner(System.in);
+        while (isChecked){
+            System.out.println("xin moi chon");
+            int choose = sc.nextInt();
+            switch (choose){
+                case 1:
+                    Student student = new Student();
+                    student.input();
+                    addStudent(student);
+                    showAll();
+                    break;
+                case 2:
+                    removeStudentById();
+                    break;
+                case 3:
+                    findStudentById();
+                    break;
+                case 4:
+                    sortByMark();
+                    break;
+                case 5:
+                    isChecked = false;
+
+            }
+        }
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import Controller.Controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Controller controller = new Controller();
+        controller.menuController();
+
+    }
+
+}
